b9d2270a1b788bcd14c4a159e2c90568e0bc03fc
geriramadansyah <128631043+geriramadansyah@users.noreply.github.com>
2026-06-30 21:06:13 +0700

    Update script.js

--- app/script.js
+++ app/script.js
@@ -1,5 +1,5 @@
 
-const URL = "https://script.google.com/macros/s/AKfycbzc7YI8_v6i8XR-dOshBESSgehdMEbwxgPPE1EjBE1dvHPL6Ed5Xyc2rs6d2e_8No1S/exec";
+const URL = "https://script.google.com/macros/s/AKfycbzEbZA_c4QgOKqr-fHORL2PxErWUIVYMJOaw3E1yIagnqqwvm2xeZSA7zDiMgnYpgtD/exec";
 
 let allData = [];
 let loading = false;
